9407db0dd158385e9efa35ffca7a8809343bc475
Richie Lee <richie@bzzzt.io>
2020-10-06 14:05:03 +0100

    select from list of variable groups

--- Public/Get-DreepyMissingEnvVars.ps1
+++ Public/Get-DreepyMissingEnvVars.ps1
@@ -1,24 +1,18 @@
 Function Get-DreepyMissingEnvVars {
-
+    param(
+        $VariableGroup
+    )
     $missingEnvVars = @()
-
-    $listHash = $list | ConvertFrom-Json -AsHashtable
-    foreach ($variableGroup in ($listHash.variableGroups)) {
-        $message = "{0} found. Checking if environment variables exist..." -f $VariableGroup.name
+    $message = "{0} found. Checking if environment variables exist..." -f $VariableGroup.name
+    Write-Host $message
+    $variableGroup.variables.GetEnumerator() | ForEach-Object {
+        $message = '{0} is in Variable Group {1}. Checking if Environment Variable exists...' -f $_.key, $VariableGroup.name
         Write-Host $message
-        $variableGroup.variables.GetEnumerator() | ForEach-Object {
-            $message = '{0} is in Variable Group {1}. Checking if Environment Variable exists...' -f $_.key, $VariableGroup.name
-            Write-Host $message
-            $varname = $_.key
-            Get-ChildItem Env:$varname -ErrorVariable missing -ErrorAction SilentlyContinue | Out-Null
-            if ($missing) {
-                $missingEnvVars = $missingEnvVars + $_.key
-            }
+        $varname = $_.key
+        Get-ChildItem Env:$varname -ErrorVariable missing -ErrorAction SilentlyContinue | Out-Null
+        if ($missing) {
+            $missingEnvVars = $missingEnvVars + "VariableGroup - {0} VariableName - {1}" -f $VariableGroup.name, $_.key
         }
     }
-    if ($missingEnvVars.count -gt 0) {
-        Write-Host "The following secrets in Variable Group envvars do not have environment variables set for them. "
-        $missingEnvVars
-        Throw
-    }
+    Return $missingEnvVars
 }
