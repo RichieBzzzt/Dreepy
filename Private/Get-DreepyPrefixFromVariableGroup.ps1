eaee5cd49c501c135fc378149726cba14d9fff0c
Richie Lee <richie@bzzzt.io>
2020-10-06 16:00:21 +0100

    more changes

--- Private/Get-DreepyPrefixFromVariableGroup.ps1
+++ Private/Get-DreepyPrefixFromVariableGroup.ps1
@@ -1,6 +1,13 @@
-Function Get-DreepyPrefixFromVariableGroup{
+Function Get-DreepyPrefixFromVariableGroup {
     param($variableGroup)
 
-    $prefix = $variableGroup.variables.Item('prefix')
-    Return $prefix
+    $prefix = $variableGroup.variables.Item('dreepyprefix')
+    if ($null -eq $prefix) {
+        $message = "Variable Group {0} does not contain a variable named 'dreepyprefix'. Add a variable called 'dreepyprefix' to Variable Group {0} and set the value to the prefix that all environment variables should have." -f $variableGroup.name
+        Write-Error $message
+        Throw
+    }
+    else {
+        Return $prefix
+    }
 }
